59886ee29414c2fa2cbf521bf87241ea281f80f8
Lady Trell <antrellkent@gmail.com>
2022-04-16 09:50:14 -0400

    Intern class methods and properties

---
+++ test/Engineer.test.js
@@ -0,0 +1,37 @@
+const Engineer = require('../lib/Engineer');
+
+describe('Engineer class', () => {
+  describe('Initialization', () => {
+        it("should create a Engineer object with 'name', 'id', 'email', 'github'", () => {
+        //Create an Engineer
+        const engineer = new Engineer('Jane Doe', '415683', 'jane.doe@company.com', 'doe-jane');
+
+        expect(engineer.name).toEqual('Jane Doe'); 
+        expect(engineer.id).toEqual('415683'); 
+        expect(engineer.email).toEqual('jane.doe@company.com');        
+        expect(engineer.github).toEqual('doe-jane');
+        });
+    });
+ 
+    describe('getRole', () => {
+        it('return employee Role', () => {
+        //Create an Engineer
+        const engineer = new Engineer('Jane Doe', '415683', 'jane.doe@company.com', 'doe-jane');
+        let value = engineer.getRole();
+
+        expect(value).toEqual('Engineer');
+        });
+    });
+ 
+    describe('getGithub', () => {
+        it('return Engineer github user name', () => {
+        //Create an Engineer
+        const engineer = new Engineer('Jane Doe', '415683', 'jane.doe@company.com', 'doe-jane');
+        let value = engineer.getGithub();
+
+        expect(value).toEqual('doe-jane');
+        });
+    });
+  
+  
+});
---
+++ test/Intern.test.js
@@ -0,0 +1,48 @@
+const Intern = require('../lib/Intern');
+
+describe('Intern class', () => {
+  describe('Initialization', () => {
+        it("should create a Intern object with 'name', 'id', 'email', 'github', 'school'", () => {
+        //Create an Intern
+        const intern = new Intern('Jane Doe', '415683', 'jane.doe@company.com', 'doe-jane', 'North Carolina Agricultural and Technical State University', 'North Carolina Agricultural and Technical State University');
+
+        expect(intern.name).toEqual('Jane Doe'); 
+        expect(intern.id).toEqual('415683'); 
+        expect(intern.email).toEqual('jane.doe@company.com');        
+        expect(intern.github).toEqual('doe-jane');        
+        expect(intern.school).toEqual('North Carolina Agricultural and Technical State University');
+        });
+    });
+ 
+    describe('getRole', () => {
+        it('return employee\'s Role', () => {
+        //Create an Intern
+        const intern = new Intern('Jane Doe', '415683', 'jane.doe@company.com', 'doe-jane', 'North Carolina Agricultural and Technical State University');
+        let value = intern.getRole();
+
+        expect(value).toEqual('Intern');
+        });
+    });
+ 
+    describe('getGithub', () => {
+        it('return Intern\'s github user name', () => {
+        //Create an Intern
+        const intern = new Intern('Jane Doe', '415683', 'jane.doe@company.com', 'doe-jane', 'North Carolina Agricultural and Technical State University');
+        let value = intern.getGithub();
+
+        expect(value).toEqual('doe-jane');
+        });
+    });
+ 
+    describe('getSchool', () => {
+        it('return Intern\'s school', () => {
+        //Create an Intern
+        const intern = new Intern('Jane Doe', '415683', 'jane.doe@company.com', 'doe-jane', 'North Carolina Agricultural and Technical State University');
+        let value = intern.getSchool();
+
+        expect(value).toEqual('North Carolina Agricultural and Technical State University');
+        });
+    });
+  
+  
+});
